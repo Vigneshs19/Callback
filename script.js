f7d79915dcbc592b729989763fd4e118c385c69f
Vigneshs19 <114480451+Vigneshs19@users.noreply.github.com>
2023-07-21 00:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+var integer = document.createElement("span");
+integer.innerHTML=10;
+document.body.append(integer);
+document.body.append(document.createElement("br"));
+document.body.append(document.createElement("br"));
+var integer2 =document.createElement("span");
+integer2.innerHTML = 9;
+setTimeout(()=> {
+document.body.append(integer2);
+document.body.append(document.createElement("br"));
+document.body.append(document.createElement("br"));
+var integer3 =document.createElement("span");
+integer3.innerHTML = 8;
+setTimeout(()=> { 
+    document.body.append(integer3);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+    var integer4 =document.createElement("span");
+integer4.innerHTML = 7;
+setTimeout(()=> { 
+    document.body.append(integer4);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+    var integer5 =document.createElement("span");
+integer5.innerHTML = 6;
+setTimeout(()=> { 
+    document.body.append(integer5);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+    var integer6 =document.createElement("span");
+integer6.innerHTML = 5;
+setTimeout(()=> { 
+    document.body.append(integer6);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+    var integer7 =document.createElement("span");
+integer7.innerHTML = 4;
+setTimeout(()=> { 
+    document.body.append(integer7);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+    var integer8 =document.createElement("span");
+integer8.innerHTML = 3;
+setTimeout(()=> { 
+    document.body.append(integer8);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+    var integer9 =document.createElement("span");
+integer9.innerHTML = 2;
+setTimeout(()=> { 
+    document.body.append(integer9);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+    var integer10 =document.createElement("span");
+integer10.innerHTML = 1;
+setTimeout(()=> { 
+    document.body.append(integer10);
+    document.body.append(document.createElement("br"));
+    document.body.append(document.createElement("br"));
+var text =document.createElement("span");
+text.innerHTML="HAPPY INDEPENDENCE DAY";
+setTimeout(()=>{
+    document.body.append(text);},1000);
+},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);},1000);
